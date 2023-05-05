7336aada6e95b14e560f1bcb843ba8ddf5458d07
L8X <49875377+L8X@users.noreply.github.com>
2023-05-05 21:45:16 +0100

    Create logger.js
    Signed-off-by: L8X <49875377+L8X@users.noreply.github.com>

---
+++ logger.js
@@ -0,0 +1,26 @@
+const net = require('net');
+const fs = require('fs');
+
+const port = 1337;
+const logFilePath = 'log.txt';
+const loggedIPs = new Set();
+
+const server = net.createServer((socket) => {
+  const { remoteAddress } = socket;
+  
+  if (!loggedIPs.has(remoteAddress)) {
+    loggedIPs.add(remoteAddress);
+    
+    const logEntry = `${new Date().toISOString()} - ${remoteAddress}\n`;
+
+    console.log(logEntry);
+
+    fs.appendFile(logFilePath, logEntry, (err) => {
+      if (err) throw err;
+    });
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
